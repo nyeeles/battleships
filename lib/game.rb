342ed840fc4540617504a640567ce02935105ed0
Nic Yeeles <n.yeeles@gmail.com>
2014-06-05 19:16:02 +0100

    Adds Ship instances to the Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,6 @@
 require_relative 'player'
 require_relative 'grid'
+require_relative 'ship'
 
 class Game
 
@@ -10,6 +11,9 @@ class Game
 		@p1_tracker_grid = Grid.new
 		@p2_primary_grid = Grid.new
 		@p2_tracker_grid = Grid.new
+		@patrol_boat = Ship.new(2)
+		@destroyer = Ship.new(3)
+		@battleship = Ship.new(4)
 	end
 
 	attr_accessor :player_1
@@ -18,5 +22,8 @@ class Game
 	attr_accessor :p1_tracker_grid
 	attr_accessor :p2_primary_grid
 	attr_accessor :p2_tracker_grid
+	attr_accessor :patrol_boat
+	attr_accessor :destroyer
+	attr_accessor :battleship
 
 end
