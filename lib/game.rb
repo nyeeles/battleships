bc91ea11b8c879a14ae2d74bbab63194291ae9d4
Nic Yeeles <n.yeeles@gmail.com>
2014-06-05 19:04:00 +0100

    Game initializes with 2 players and 4 grids

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,14 @@
-require 'player'
-require 'grid'
+require_relative 'player'
+require_relative 'grid'
 
 class Game
 
-	def initialize
-	@playas = [playa_1 = Playa.new, playa_2 = Playa.new]	
+	def initialize 
+		@playa_1 = Playa.new
+		@playa_2 = Playa.new
+		@p1_primary_grid = Grid.new
+		@p1_tracker_grid = Grid.new
+		@p2_primary_grid = Grid.new
+		@p2_tracker_grid = Grid.new
 	end
-
-	attr_reader :playas
 end
